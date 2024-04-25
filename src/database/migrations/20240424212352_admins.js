61d7aa12e9b6125a17fe414bd0e53b2cd8eef611
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2024-04-25 03:02:44 -0300

    Implementando autenticação e criptografia

---
+++ src/database/migrations/20240424212352_admins.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('admins', function (table) {
+        table.string('username').primary();
+        table.string('password').notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('admins')
+};
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const CrudController = require('./controllers/crudController');
 const adminController = require('./controllers/admin');
 const routesController = require('./controllers/routesController');
 const noticesController = require('./controllers/noticesController');
-
+const login = require('./middleware/admin_login')
 const routes = express.Router();
 
 routes.get('/GetAll', CrudController.GetAll);
@@ -15,14 +15,14 @@ routes.delete('/DeleteAlunos/:id', CrudController.Delete);
 routes.post('/administrador/login', adminController.login);
 routes.post('/administrador/insert', adminController.create);
 
-routes.post('/administrador/insert-route', routesController.create);
+routes.post('/administrador/insert-route',login, routesController.create);
 routes.get('/getAllRoutes', routesController.GetAll);
-routes.put('/administrador/update-route/:id', routesController.update);
-routes.delete('/administrador/delete-route/:id', routesController.delete);
+routes.put('/administrador/update-route/:id', login,routesController.update);
+routes.delete('/administrador/delete-route/:id', login ,routesController.delete);
 
-routes.post('/administrador/insert-notice', noticesController.create);
+routes.post('/administrador/insert-notice', login, noticesController.create);
 routes.get('/getAllNotices', noticesController.GetAll);
-routes.put('/administrador/update-notice/:id', noticesController.update);
-routes.delete('/administrador/delete-notice/:id', noticesController.delete);
+routes.put('/administrador/update-notice/:id', login, noticesController.update);
+routes.delete('/administrador/delete-notice/:id', login, noticesController.delete);
 
 module.exports = routes;
